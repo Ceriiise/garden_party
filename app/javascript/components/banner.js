47432a2321525a2cc0ccd77ac0eaa5a9b4e94d3c
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-11-28 16:39:33 +0100

    fonts and colors v3

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Best garden for your party"],
+    strings: ["Find the best garden for your party"],
     typeSpeed: 60,
     loop: true
   });
