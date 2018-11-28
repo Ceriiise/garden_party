e3ea7d3550c302e4b692de75476a95903ef13af2
François HAMEAU <f.hameau@gmail.com>
2018-11-28 10:19:42 +0100

    pundit map toggle

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -1,1 +1,13 @@
-// TODO: Write your JS code in here
+
+const displayMap = () => {
+  const btn = document.querySelector('#map-btn');
+  const map = document.querySelector('#map');
+  const cards = document.querySelector(".card-container")
+
+  btn.addEventListener('click', () => {
+    map.classList.toggle('disabled');
+    cards.classList.toggle('big');
+  });
+};
+
+export { displayMap };
