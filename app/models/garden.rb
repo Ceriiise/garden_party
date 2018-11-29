b30b9e1a173c9cd67fe3b2f36dd058dc5032efb4
François HAMEAU <f.hameau@gmail.com>
2018-11-29 16:24:31 +0100

    search done

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -12,4 +12,11 @@ class Garden < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  # include PgSearch
+  # pg_search_scope :search_by_name_address_barbecue_and_swimmining_pool,
+  #   against: [:name, :address, :barbecue, :swimming_pool],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
