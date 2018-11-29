3671221c60037194ef0c8a054a4cf22f23125e78
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-11-29 22:37:59 +0100

    a lot of animations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import "../plugins/flatpickr"
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import { displayMap } from '../components/map';
-import { loadDynamicBannerText } from '../components/banner';
+// import { loadDynamicBannerText } from '../components/banner';
 import { activeLinks } from '../components/navbar';
 // import { initAutocomplete } from '../plugins/init_autocomplete';
 
