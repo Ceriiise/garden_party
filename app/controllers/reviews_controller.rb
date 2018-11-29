b1485498537a9c75cd595f2f16901a8101f8e4c9
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-11-29 15:22:01 +0100

    add book forms to shows

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,7 +14,7 @@ class ReviewsController < ApplicationController
     @review.user = current_user
     authorize @booking
     if @review.save
-      redirect_to booking_path(@booking)
+      redirect_to bookings_path
     else
       render "bookings/show"
     end
